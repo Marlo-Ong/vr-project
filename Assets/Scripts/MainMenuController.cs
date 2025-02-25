14817965bcca59a22a50cdbd046d242f2fee59d8
ploy <ployw@unr.edu>
2025-02-24 20:41:07 -0800

    add tutorial panel + remove getItemEffect pkg due to compiler errors

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,18 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+  public GameObject menuPanel;
+  public GameObject tutorialPanel;
+
   public Button startButton;
   public Button exitButton;
+  public Button returnButton;
+  public Button tutorialButton;
 
   void Start()
   {
-      startButton.onClick.AddListener(StartGame);
-      exitButton.onClick.AddListener(ExitGame);
+    EnableMenuPanel();
+
+    startButton.onClick.AddListener(StartGame);
+    exitButton.onClick.AddListener(ExitGame);
+    returnButton.onClick.AddListener(EnableMenuPanel);
+    tutorialButton.onClick.AddListener(EnableTutorialPanel);
   }
 
   public void StartGame()
@@ -22,6 +29,20 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
   public void ExitGame()
   {
+    // works in build, not in unity editor play test
+    Debug.Log("Exit only works in build.");
     Application.Quit();
+  }
+
+  public void EnableTutorialPanel()
+  {
+    menuPanel.SetActive(false);
+    tutorialPanel.SetActive(true);
+  }
+
+  public void EnableMenuPanel()
+  {
+    tutorialPanel.SetActive(false);
+    menuPanel.SetActive(true);
   }
 }
